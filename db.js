3d2e25ce760ec41d889e33664ed45d12e630c1b0
zitro <ajosueramos@gmail.com>
2025-06-01 00:24:39 -0500

    Finished Project

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'database_crud_node',
+  database: 'DATABASE crud_node',
   password: '12345',
   port: 5432,
 });
